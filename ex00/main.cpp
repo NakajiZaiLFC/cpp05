e243ba1354cc3815dd73712a381e6a22d38f60cb
snakajim <snakajim@student.42tokyo.jp>
2025-11-19 21:04:34 +0900

    ok

---
+++ ex00/main.cpp
@@ -0,0 +1,23 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int main(void) {
+	try {
+		Bureaucrat Tom("Tom", 3);
+		std::cout << Tom << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat Jack("Jack", 0);
+		std::cout << Jack << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat Jack("Kyle", 1000);
+		std::cout << Jack << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
